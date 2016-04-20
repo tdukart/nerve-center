a06d6d85c25e8b0b656a689bd7da9c0b7b201c5f
Todd Dukart <tdukart@aloftinteractive.com>
2016-04-19 20:30:01 -0400

    #2 Add dataPoint methods

--- test/test-nerve-center.js
+++ test/test-nerve-center.js
@@ -1,7 +1,7 @@
 /* global __karma__ */
 var NerveCenter = require( 'nerve-center' );
 
-describe( 'NerveCenter', function () {
+describe( 'NerveCenter messaging', function () {
 
 	it( 'triggers a callback when a message is broadcast', function () {
 
@@ -67,6 +67,111 @@ describe( 'NerveCenter', function () {
 		expect( callback3 ).toHaveBeenCalled();
 		expect( callback4 ).toHaveBeenCalled();
 		expect( callback5 ).toHaveBeenCalled();
+
+	} );
+
+} );
+
+describe( 'NerveCenter data points', function () {
+
+	it( 'allows data to be set and retrieved', function () {
+
+		var nc = new NerveCenter();
+
+		nc.setDataPoint( 'greeting', 'howdy' );
+		var returnValue = nc.getDataPoint( 'greeting' );
+
+		expect( returnValue ).toBe( 'howdy' );
+
+	} );
+
+	it( 'enforces data point types', function () {
+
+		var nc = new NerveCenter();
+
+		nc.initializeDataPoint( 'shouldBeAString', 'string' );
+
+		expect( function () {
+			nc.setDataPoint( 'shouldBeAString', { thisIs: 'not a string' } );
+		} ).toThrowError( TypeError );
+
+	} );
+
+	it( 'allows data subscriptions to be alerted to changes in data', function () {
+
+		var nc = new NerveCenter();
+
+		nc.initializeDataPoint( 'greeting', 'string', 'howdy' );
+
+		var callback = jasmine.createSpy( 'callback' );
+		nc.subscribeToDataPoint( 'greeting', callback );
+
+		nc.setDataPoint( 'greeting', 'hola' );
+
+		expect( callback ).toHaveBeenCalledWith( jasmine.objectContaining( {
+			oldValue: 'howdy',
+			newValue: 'hola'
+		} ) );
+
+	} );
+
+	it( 'allows data subscriptions to be bound before data is set', function () {
+
+		var nc = new NerveCenter();
+
+		var callback = jasmine.createSpy( 'callback' );
+		nc.subscribeToDataPoint( 'greeting', callback );
+
+		nc.setDataPoint( 'greeting', 'howdy' );
+
+		expect( callback ).toHaveBeenCalledWith( jasmine.objectContaining( {
+			newValue: 'howdy'
+		} ) );
+
+	} );
+
+	it( 'allows multiple data subscriptions', function () {
+
+		var nc = new NerveCenter();
+
+		var callback1 = jasmine.createSpy( 'callback' );
+		var callback2 = jasmine.createSpy( 'callback' );
+		var callback3 = jasmine.createSpy( 'callback' );
+		var callback4 = jasmine.createSpy( 'callback' );
+		var callback5 = jasmine.createSpy( 'callback' );
+
+		nc.subscribeToDataPoint( 'greeting', callback1 );
+		nc.subscribeToDataPoint( 'greeting', callback2 );
+		nc.subscribeToDataPoint( 'greeting', callback3 );
+		nc.subscribeToDataPoint( 'greeting', callback4 );
+		nc.subscribeToDataPoint( 'greeting', callback5 );
+
+		nc.setDataPoint( 'greeting', 'bonjour' );
+
+		expect( callback1 ).toHaveBeenCalled();
+		expect( callback2 ).toHaveBeenCalled();
+		expect( callback3 ).toHaveBeenCalled();
+		expect( callback4 ).toHaveBeenCalled();
+		expect( callback5 ).toHaveBeenCalled();
+
+	} );
+
+	it( 'allows namespaced data subscriptions', function () {
+
+		var nc = new NerveCenter();
+
+		var unnamespacedCallback = jasmine.createSpy( 'callback' );
+		var namespacedCallback = jasmine.createSpy( 'callback' );
+
+		nc.subscribeToDataPoint( 'greeting', unnamespacedCallback );
+		nc.subscribeToDataPoint( 'ns.greeting', namespacedCallback );
+
+		nc.unsubscribeFromDataPoint( 'ns.greeting' );
+
+		nc.setDataPoint( 'greeting', 'aloha' );
+
+		expect( unnamespacedCallback ).toHaveBeenCalled();
+		expect( namespacedCallback ).not.toHaveBeenCalled();
 
 	} );
 
